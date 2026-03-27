5098491a70f77e4ebe1e6e13c5cb365f4574256e
JakeAduana <aduananorwin@gmail.com>
2026-03-27 16:20:32 +0800

    mobapp

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Jake Norwin Aduana</Text>
-      <StatusBar style="auto" />
+      <Button title = "Tap Me!"/>
     </View>
   );
 }
